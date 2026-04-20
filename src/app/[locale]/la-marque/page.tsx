699f003422985b01d86a874105a69df64ff9f171
Axel Binda <axel.binda@raliance.com>
2026-04-20 11:08:34 +0200

    Add multilingual content and imagery for "Le Calvaire" across domain pages

--- src/app/[locale]/la-marque/page.tsx
+++ src/app/[locale]/la-marque/page.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import { getTranslations } from 'next-intl/server';
 import { HeroImage } from '@/app/components/HeroImage/HeroImage';
+import { Text } from '@/app/components/Text/Text';
 
 export default async function DomainePage() {
   const t = await getTranslations('brand');
@@ -7,6 +9,25 @@ export default async function DomainePage() {
   return (
     <div>
       <HeroImage src="/img/calvaire.webp" title={t('title')} />
+
+      <Text align="center" textSize="lg">
+        {t('intro')}
+      </Text>
+
+      <div className="relative w-1/2 h-[75vh] m-auto mb-50">
+        <Image
+          alt={t('alt')}
+          layout="fill"
+          objectFit="cover"
+          src="/img/grape.webp"
+        />
+      </div>
+
+      <Text align="center">
+        {t.rich('outro', {
+          br: () => <br />,
+        })}
+      </Text>
     </div>
   );
 }
