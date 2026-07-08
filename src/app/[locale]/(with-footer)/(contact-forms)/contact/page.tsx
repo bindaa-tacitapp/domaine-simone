6ffeb7f0a04cf6b4cbba301543f184a571fe55c7
Axel Binda <axel.binda@raliance.com>
2026-07-08 21:48:04 +0200

    feat(contact-page): add metadata generation for alternate canonical URLs [SIM-xxxx]

--- src/app/[locale]/(with-footer)/(contact-forms)/contact/page.tsx
+++ src/app/[locale]/(with-footer)/(contact-forms)/contact/page.tsx
@@ -1,12 +1,29 @@
+import { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
 import { ContentWrapper } from '@/components/ContentWrapper/ContentWrapper';
 import { FormContact } from '@/components/forms/FormContact';
-import { ContactType } from '@/constants/routes';
+import { ContactType, ROUTES } from '@/constants/routes';
+import { Locale } from '@/i18n/config';
+import { getPathname } from '@/i18n/navigation';
 import { cn } from '@/libs/cn';
 
 type Props = {
   searchParams: Promise<{ type: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+
+  return {
+    alternates: {
+      canonical: getPathname({ href: ROUTES.contact, locale }),
+    },
+  };
+}
 
 export default async function ContactPage({ searchParams }: Props) {
   const [t, { type }] = await Promise.all([
